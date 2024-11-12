a3a9e495c2aa5e83c901490a8066ae11e9f2c5cc
Adarsh <ashu2100ag@gmail.com>
2024-11-12 18:19:45 +0530

    responsive design

--- Software Code/FireStore/TestPhaseCode/src/CollectionCards/AcCard.jsx
+++ Software Code/FireStore/TestPhaseCode/src/CollectionCards/AcCard.jsx
@@ -3,7 +3,7 @@ import Loading from "./Loading";
 import Card from "../Card";
 export default function AcCard({ acsData, CollectionName }) {
   return (
-    <div className="grid grid-cols-1 w-[60vw] m-auto mt-8 mb-10 gap-12 xs:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 xxl:grid-cols-6">
+    <div className="grid grid-cols-1 w-[60vw] m-auto mt-8 mb-10 gap-12 xs:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
       {acsData ? (
         acsData.map((ac) => (
           <Card
--- Software Code/FireStore/TestPhaseCode/src/CollectionCards/FanCard.jsx
+++ Software Code/FireStore/TestPhaseCode/src/CollectionCards/FanCard.jsx
@@ -3,7 +3,7 @@ import Card from "../Card";
 import Loading from "./Loading";
 export default function FanCard({ fansData, CollectionName }) {
   return (
-    <div className="grid grid-cols-1 w-[60vw] m-auto mt-8 mb-10 gap-12 xs:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 xxl:grid-cols-6">
+    <div className="grid grid-cols-1 w-[60vw] m-auto mt-8 mb-10 gap-12 xs:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
       {fansData ? (
         fansData.map((fan) => ( 
           <Card
