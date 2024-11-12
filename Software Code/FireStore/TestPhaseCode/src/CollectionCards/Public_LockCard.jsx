fec7d5803c247f690383d068dd8f533b6a4f0da1
Adarsh <ashu2100ag@gmail.com>
2024-11-12 18:53:18 +0530

    improved responisveness

--- Software Code/FireStore/TestPhaseCode/src/CollectionCards/Public_LockCard.jsx
+++ Software Code/FireStore/TestPhaseCode/src/CollectionCards/Public_LockCard.jsx
@@ -3,7 +3,7 @@ import Loading from "./Loading";
 import Card from "../Card";
 export default function Public_LockCard({publicDoorLockData,CollectionName}) {
   return (
-    <div className="grid grid-cols-1 w-[60vw] m-auto mt-8 mb-10 gap-12 xs:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+    <div className="grid grid-cols-1 w-[80vw] m-auto mt-8 mb-10 gap-12 xs:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
       {publicDoorLockData ? (
         publicDoorLockData.map((Lock) => (
           <Card
